75e5f1f767b8a040f05255337c32652ad1a50583
Cysteine12 <gbengapeace2001@gmail.com>
2022-08-01 19:41:16 +0100

    fix cors

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(
     cors({
-        origin: '*'
+        origin: process.env.ORIGIN_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
     })
 )
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    next()
-})
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     next()
+// })
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 app.use(session({
